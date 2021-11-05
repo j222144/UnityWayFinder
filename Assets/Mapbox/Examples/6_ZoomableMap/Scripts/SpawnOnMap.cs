9026f14db1f15b3df4aa84fa6fef39cf8ab2330b
AxelOliver <axeloliver300@gmail.com>
2021-11-05 14:19:00 +0800

    Added pathfinding algorithm to line
    Updated line every frame
    Refactored some code
    added pathfinding algorithm
    added JSON with points

--- Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -6,75 +6,72 @@
 	using Mapbox.Unity.MeshGeneration.Factories;
 	using Mapbox.Unity.Utilities;
 	using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using System.Linq;
 
-	public class SpawnOnMap : MonoBehaviour
+    public class SpawnOnMap : MonoBehaviour
 	{
 
 		[SerializeField]
 		AbstractMap _map;
 
-		[SerializeField]
-		[Geocode]
-		string[] _locationStrings;
-		Vector2d[] _locations;
-
-		[SerializeField]
-		float _spawnScale = 100f;
-
-		[SerializeField]
-		GameObject _markerPrefab;
-
-		List<GameObject> _spawnedObjects;
+		public LineRenderer line;
 		List<Vector3> pos = new List<Vector3>();
+		PathFinder pathFinder = new PathFinder();
 
-
+		// Json
+		TextAsset targetFile;
+		string myJson;
+		Root pathFinding;
 		void Start()
 		{
-			_locations = new Vector2d[_locationStrings.Length];
-			_spawnedObjects = new List<GameObject>();
-			for (int i = 0; i < _locationStrings.Length; i++)
-			{
-				var locationString = _locationStrings[i];
-				_locations[i] = Conversions.StringToLatLon(locationString);
-				var instance = Instantiate(_markerPrefab);
-				instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
-				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-				_spawnedObjects.Add(instance);
-			}
+			// Assign JSON
+			targetFile = Resources.Load<TextAsset>("NavPoints");
+			myJson = targetFile.text;
+			pathFinding = JsonConvert.DeserializeObject<Root>(myJson);
+			// Create LineRenderer
+			LineRenderer line = gameObject.AddComponent<LineRenderer>();
+			line.material = new Material(Shader.Find("Sprites/Default"));
+			line.startWidth = 1f;
+			line.endWidth = 1f;
 		}
 
 		private void Update()
 		{
-			int count = _spawnedObjects.Count;
-
-			LineRenderer l = gameObject.AddComponent<LineRenderer>();
-			for (int i = 0; i < count; i++)
-			{
-				var spawnedObject = _spawnedObjects[i];
-				var location = _locations[i];
-				spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
-				spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-				//
-				l.startWidth = 1f;
-				l.endWidth = 1f;
-				pos.Add(new Vector3(spawnedObject.transform.localPosition.x, 2f, spawnedObject.transform.localPosition.z));
-				
-				l.positionCount = pos.Count;
-				
-				l.SetPositions(pos.ToArray());
-				l.useWorldSpace = true;
-			}
-
+			DrawLine();
 		}
 
 
+		// Update line every frame incase map moves
 		private void DrawLine()
         {
-			
-			
+			LineRenderer line = GetComponent<LineRenderer>();
+			pos.Clear();
+			//line = Instantiate (line) as LineRenderer;
+			//if (gameObject.GetComponent<LineRenderer>())
+			//{
+			//	//l = gameObject.GetComponent<LineRenderer>();
+			//	Destroy(GetComponent<LineRenderer>()); // if already drawn, destroy and redraw
+			//}
 
+			//l = gameObject.AddComponent<LineRenderer>();
+			List<Feature> shortestPath = pathFinder.ShortestPathFunction(pathFinding.features, pathFinding.features.Where(point => point.properties.roomNumber == "Main").Single(), pathFinding.features.Where(point => point.properties.roomNumber == "B244").Single());
+            foreach (var feature in shortestPath)
+            {
+				Vector2d conversion = Conversions.StringToLatLon($"{feature.geometry.coordinates[1]}, {feature.geometry.coordinates[0]}");
+				Vector3 localPos = _map.GeoToWorldPosition(conversion, true);
+				pos.Add(new Vector3(localPos.x, 2f, localPos.z));
+            }
+			//foreach (var location in _locations)
+			//{
+			//	Vector3 localPos = _map.GeoToWorldPosition(location, true);
+			//	//spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+			//	//transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+			//	//
+			//	pos.Add(new Vector3(localPos.x, 2f, localPos.z));
+			//}
+			line.positionCount = pos.Count;
+			line.SetPositions(pos.ToArray());
 		}
-
-		
 	}
 }
